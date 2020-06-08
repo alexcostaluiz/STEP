8189a8ad53c4dead3fcec2b18515ec39287dfafc
Alexander Costa <alexcostaluiz@outlook.com>
2020-06-08 07:06:03 +0000

    Add javadoc documentation and comments

--- portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
@@ -1,3 +1,12 @@
+/**
+ * ListCommentsServlet.java
+ * 06/05/2020
+ *
+ * An endpoint at which comments and replies of a specific project page
+ * may be retrieved from Datastore and returned in JSON.
+ *
+ * @author Alexander Luiz Costa
+ */
 package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.Cursor;
@@ -16,29 +25,39 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import com.google.sps.data.Comment;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** 
- * Lists all comments. 
+ * Lists comments and replies of a specifc project page. 
  */
 @WebServlet("/list-comments")
 public class ListCommentsServlet extends HttpServlet {
+  /** The number of parent comments returned per request. */
   static final int PAGE_SIZE = 5;
 
+  /**
+   * Aids in the serialization of comment data to JSON through Gson.
+   */
   private class Response {
+    /** The comments to return. */
     List<Comment> comments;
+    
+    /** The replies associated with the comments being returned. */
     List<Comment> replies;
+    
+    /** A cursor pointing to the last retrieved comment. */
     String cursor;
 
+    /** 
+     * Constructs the response of this endpoint.
+     */
     public Response(List<Comment> comments,
                     List<Comment> replies,
                     String cursor) {
@@ -54,11 +73,13 @@ public class ListCommentsServlet extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     FetchOptions fetchOptions = FetchOptions.Builder.withLimit(PAGE_SIZE);
 
+    // If a cursor is specified in the request, fetch the next PAGE_SIZE comments.
     String startCursor = request.getParameter("cursor");
     if (startCursor != null) {
       fetchOptions.startCursor(Cursor.fromWebSafeString(startCursor));
     }
-    
+
+    // Use the referring url to determine which project's comments are retrieved.
     String referer = request.getHeader("referer");
     String project = referer.substring(referer.lastIndexOf('/') + 1);
     if (project.equals("projects")) {
@@ -66,11 +87,14 @@ public class ListCommentsServlet extends HttpServlet {
     }
     List<String> projects = Arrays.asList("ugadining", "portflagship",
                                           "3dmodeling", "visualizations");
+
+    // Reject unrecognized referring urls.
     if (!projects.contains(project)) {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
       return;
     }
-    
+
+    // Fetch the next PAGE_SIZE parent comments for the determined project.
     Filter projectFilter = new FilterPredicate("project", FilterOperator.EQUAL, project);
     Filter commentFilter = new FilterPredicate("parentId", FilterOperator.EQUAL, -1);
     CompositeFilter compositeFilter = CompositeFilterOperator.
@@ -98,6 +122,7 @@ public class ListCommentsServlet extends HttpServlet {
       
     }
 
+    // Fetch any replies associated with the PAGE_SIZE parent comments already fetched.
     List<Comment> replies = new ArrayList<>();
     if (!parentIds.isEmpty()) {
       Filter parentFilter = new FilterPredicate("parentId", FilterOperator.IN, parentIds);
